332d8c3f9632ca7615d5bed29a85278965bcfe87
Kasia Goj <katarzyna.goj@fundingbox.com>
2020-10-12 18:56:51 +0200

    using the textContent property

---
+++ 30-js-dodawanie-tekstu/js/main.js
@@ -0,0 +1,7 @@
+/* 3 . W pliku main.js stworz zmienną o dowolnej nazwie i przypisz do niej stworzony wcześniej paragraf (przy użyciu document.querySelector).
+
+4. Następnie dodaj do paragrafu dowolny tekst wykorzystując właściwość textContent */
+
+let myParagraph = document.querySelector('p');
+
+myParagraph.textContent = 'Dowolny tekst wykorzystujący właściwość textContent'
