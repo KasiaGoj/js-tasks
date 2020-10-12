1a2155de2b09c3cc8324e2f3c0e3d6257fe2ac40
Kasia Goj <katarzyna.goj@fundingbox.com>
2020-10-12 18:43:22 +0200

    numbers added

---
+++ 29-js-suma-zmiennych/js/main.js
@@ -0,0 +1,19 @@
+/* 2. W pliku main.js zdeklaruj dwie zmienne o nazwie:
+- "firstNumber"
+- "secondNumber"
+
+3. Przypisz do nich dowolne liczby.
+4. Następnie napisz funkcję, która przyjmuje dwa parametry - stworzone przez Ciebie zmienne.
+5. Ma ona zwracać (return) sumę tych parametrów. */
+
+let firstNumber = 4085;
+
+let secondNumber = 198;
+
+
+function sumOfNumbers (firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+
+/* console.log(sumOfNumbers(firstNumber, secondNumber)) */
