7f64cb302d35f432f597c53d762fc3cfd790968e
Kasia Goj <katarzyna.goj@fundingbox.com>
2020-10-13 20:22:42 +0200

    loger string found

---
+++ 34-js-dlugosc-stringa/js/main.js
@@ -0,0 +1,15 @@
+/* Zdeklaruj dwie zmienne o dowolnych nazwach, do których przypisz kolejne stringi:
+1. "Uwielbiam JavaScript"
+2. "Jestem świetnym programistą"
+
+Napisz funkcję, która przyjmuje stworzone zmienne jako parametry. Funkcja ma zwracać string, który jest dłuższy. Wynik działania funkcji wyświetl w konsoli. */
+
+let first = "Uwielbiam JavaScript";
+
+let second = "Jestem świetnym programistą";
+
+if (length.first > length.second) {
+    console.log(first);
+} else {
+    console.log(second);
+}
