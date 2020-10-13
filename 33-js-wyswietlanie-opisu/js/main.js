fd3352e3382c84aa97b52cec8115936e7e5a1061
Kasia Goj <katarzyna.goj@fundingbox.com>
2020-10-13 16:02:39 +0200

    added text to button

---
+++ 33-js-wyswietlanie-opisu/js/main.js
@@ -0,0 +1,16 @@
+/* W pliku main.js stwórz zmienną o dowolnej nazwie i przypisz do niej paragraf przy użyciu
+
+document.getElementById("nazwa-id");
+Następnie stwórz kolejną zmienną i przypisz do niej button (przy użyciu document.querySelector)
+
+Do buttona dodaj zdarzenie onclick, po którym zostanie wywołana funkcja.
+
+Funkcja ma dodać do paragrafu opis (dowolny tekst). */
+
+let x = document.getElementById("opis");
+
+let myButton = document.querySelector('button')
+
+myButton.onclick = function () {
+    x.textContent = 'dowolna treść'
+}
