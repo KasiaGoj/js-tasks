9dfefc9e88522548d06286a0add28248338ae587
Kasia Goj <katarzyna.goj@fundingbox.com>
2020-10-14 13:31:07 +0200

    function added

--- 32-js-wieksza-liczba/js/main.js
+++ 32-js-wieksza-liczba/js/main.js
@@ -1,8 +1,15 @@
-let num1 = 2;
-let num2 = 5;
+let number1 = 2;
+let number2 = 5;
+
+
 
 /* 2. Następnie napisz funkcję, która pobiera te dwie zmienne jako parametry.
 
 3. Funkcja zwraca większą liczbę. */
 
-console.log(Math.max (num1,num2));
+function findBigger(num1, num2) {
+    return Math.max(num1,num2);
+}
+console.log(findBigger(number1,number2));
+
+console.log(findBigger(10, 15));
